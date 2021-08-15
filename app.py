75d46b149a0ee39e52a52fa5dbd83630c4da67b6
GobKron <mankaran.parmar@gmail.com>
2021-08-15 01:47:56 -0600

    Added navbar and a few other pages.
    Added a navbar for every page and create some placeholder pages.

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
-    return "Hello! this is the main page <h1>Hello<h1>"
+    return render_template("homePage.html")
+
+@app.route('/f1')
+def forumla():
+    return render_template("formula1.html")
 
 @app.route("/weather")
 def weather():
-    return render_template("weather.html")
+    return render_template("weather0.html")
 
 
 if __name__ == "__main__":
